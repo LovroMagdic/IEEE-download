960775eee1a47c765a87d94b6597bc1d27917322
Lovro Magdic <lovro@Lovros-MacBook-Air-2.local>
2025-01-28 19:46:09 +0100

    fixed last index so it supports various results

--- last_index_test.py
+++ last_index_test.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import time, re
-from PIL import Image
+import time, re, sys
 
 def ceiling_based_on_last_digit(number):
     if number % 10 == 0:
@@ -10,24 +9,26 @@ def ceiling_based_on_last_digit(number):
 
     return (number // 10 + 1) * 10
 
-links = ["https://ieeexplore.ieee.org/xpl/conhome/10500516/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10554669/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10662982/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10483273/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10667474/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10663060/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10664657/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10654343/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10735440/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10723818/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10493069/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10542692/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10685652/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10540647/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10605587/proceeding",
-"https://ieeexplore.ieee.org/xpl/conhome/10589922/proceeding"]
+links = ["https://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText=mali%20bembo",
+         "https://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText=machine%20learning", 
+         "https://ieeexplore.ieee.org/xpl/conhome/10723818/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10500516/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10554669/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10662982/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10483273/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10667474/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10663060/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10664657/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10654343/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10735440/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10723818/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10493069/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10542692/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10685652/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10540647/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10605587/proceeding",
+         "https://ieeexplore.ieee.org/xpl/conhome/10589922/proceeding"] # 1. search result, 2. conference link, 3. no results
 
-link = "https://ieeexplore.ieee.org/xpl/conhome/10723818/proceeding"
 
 for link in links:
     chrome_options = Options()
@@ -41,20 +42,34 @@ for link in links:
     except:
         pass
     time.sleep(2)
-
-    elem = browser.find_element(By.CSS_SELECTOR, "[class*='global-margins']") ## col-24-24 Dashboard-section
-    elem = elem.get_attribute("innerHTML").splitlines()[0]
+    try:
+        elem = browser.find_element(By.CSS_SELECTOR, "[class*='Dashboard-header']") # Dashboard-header col-12
+        #elem = browser.find_element(By.CSS_SELECTOR, "[class*='global-margins']") ## col-24-24 Dashboard-section
+        elem = elem.get_attribute("innerHTML").splitlines()[0]
+        flag = 1
+    except:
+        print(f"Couldnt locate results div for link > {link}, check if valid link.")
+        sys.exit()
 
     pattern = r'>([^<]+)<'
     matches = re.findall(pattern, elem)
     cleaned_text = ' '.join(match.strip() for match in matches if match.strip())
     cleaned_text = cleaned_text.split(" ")
-    num_results = cleaned_text[-1]
-    if "," in num_results:
-        num_results = int(num_results.replace(",", ""))
-    else:
-        num_results = int(num_results)
+    
+    if "Showing" in cleaned_text and "for" in cleaned_text:
+        num_results = cleaned_text[3]
+    elif "Showing" in cleaned_text and "for" not in cleaned_text:
+        num_results = cleaned_text[-1]
+    elif "No" in cleaned_text and "results" in cleaned_text:
+        num_results = None
+        print(f"Detected no results for link > {link}")
+        flag = 0
+    if flag:
+        if "," in num_results:
+            num_results = int(num_results.replace(",", ""))
+        else:
+            num_results = int(num_results)
 
-    num_results = int(ceiling_based_on_last_digit(num_results) / 10)
+        num_results = int(ceiling_based_on_last_digit(num_results) / 10)
 
-    print(f"For this link > {link}, there are > {num_results} pages of result.")
+        print(f"For this link > {link}, there are > {num_results} pages of result.")
